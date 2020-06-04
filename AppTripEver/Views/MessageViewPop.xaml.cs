ddc586196899bf69fe651bdd662e88410452011a
mclopez09 <mclopez09@javerianacali.edu.co>
2020-06-04 03:26:20 -0500

    PopUp funciona sin mensaje

--- AppTripEver/Views/MessageViewPop.xaml.cs
+++ AppTripEver/Views/MessageViewPop.xaml.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
+using AppTripEver.ViewModels;
 using Xamarin.Forms.Xaml;
 
 namespace AppTripEver.Views
@@ -12,9 +12,11 @@ namespace AppTripEver.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MessageViewPop
     {
+        MessageViewModel context = new MessageViewModel();
         public MessageViewPop()
         {
             InitializeComponent();
+            BindingContext = context;
         }
     }
 }
