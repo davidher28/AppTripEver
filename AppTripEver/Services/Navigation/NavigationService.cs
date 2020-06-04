05645734f23e81df27a2de8e65b346d677c0ada1
davidher28 <davidher28@javerianacali.edu.co>
2020-06-03 22:58:31 -0500

    Creación navigation service y BaseViewModel

---
+++ AppTripEver/Services/Navigation/NavigationService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AppTripEver.Services.Navigation
+{
+    public class NavigationService
+    {
+        public async Task PushPage(Page page, object parameter = null)
+        {
+            if (page != null)
+            {
+                var navigationPage = Application.Current.MainPage;
+                var viewModel = page.BindingContext;
+                await ((ViewModelBase)viewModel).ConstructorAsync(parameter);
+                NavigationPage wrapper = new NavigationPage(page);
+                await ((NavigationPage)navigationPage).PushAsync(wrapper);
+            }
+        }
+    }
+}
---
+++ AppTripEver/ViewModels/BaseViewModel.cs
@@ -0,0 +1,16 @@
+﻿using AppTripEver.Services.Propagation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppTripEver.ViewModels
+{
+    public class BaseViewModel : NotificationObject
+    {
+        public virtual async Task ConstructorAsync(object parameters)
+        {
+            await Task.FromResult(true);
+        }
+    }
+}
