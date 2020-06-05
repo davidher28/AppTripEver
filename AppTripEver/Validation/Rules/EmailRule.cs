0f25a95f651a9fa9a1d8f89016cd2027b8fde3a0
German <germanc97@javerianacali.edu.co>
2020-06-04 20:41:25 -0500

    REglas de digitos y correo

---
+++ AppTripEver/Validation/Rules/EmailRule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Mail;
+using AppTripEver.Validation.Base;
+
+namespace AppTripEver.Validation.Rules
+{
+    public class EmailRule<T> : IValidationRule<T>
+    {
+        public string ValidationMessage { get; set; }
+
+        public bool Check(T value)
+        {
+            try
+            {
+                var valueString = value as string;
+                if(value != null && !string.IsNullOrWhiteSpace(valueString))
+                {
+                    MailAddress m = new MailAddress(valueString);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+    }
+}
--- AppTripEver/Validation/Rules/TenDigitsRule.cs
+++ AppTripEver/Validation/Rules/TenDigitsRule.cs
@@ -3,19 +3,25 @@ using AppTripEver.Validation.Base;
 
 namespace AppTripEver.Validation.Rules
 {
-    public class RequiredRule<T> : IValidationRule<T>
+    public class TenDigitsRule<T> : IValidationRule<T>
     {
         public string ValidationMessage { get; set; }
 
         public bool Check(T value)
         {
             bool response = false;
-            if(value != null)
+            if (value != null)
             {
                 var str = value as string;
-                response = !string.IsNullOrWhiteSpace(str);
+                if(str.Length == 10)
+                {
+                    response = true;
+                }
             }
             return response;
         }
+
+        
     }
+    
 }
