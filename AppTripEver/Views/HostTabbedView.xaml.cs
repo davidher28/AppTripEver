395d2ab25a10b2f69b27cce0cba26185177af70d
davidher28 <davidher28@javerianacali.edu.co>
2020-06-07 03:49:50 -0500

    Host Tabbes View

---
+++ AppTripEver/Views/HostTabbedView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppTripEver.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppTripEver.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HostTabbedView : TabbedPage
+    {
+        HostTabbedViewModel context = new HostTabbedViewModel();
+        public HostTabbedView()
+        {
+            InitializeComponent();
+            BindingContext = context;
+        }
+    }
+}
