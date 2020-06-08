c43d1471035c7ec743cdc56da78db19dc0e1e9d8
davidher28 <davidher28@javerianacali.edu.co>
2020-06-08 02:06:45 -0500

    Login Model

---
+++ AppTripEver/Models/LoginModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppTripEver.Models
+{
+    public class LoginModel : BaseModel
+    {
+        #region Properties
+        private string nombre;
+
+        private string contra;
+
+        #endregion Properties
+
+        #region Initialize
+
+        public LoginModel()
+        { }
+
+        #endregion Initialize
+
+        #region Getters & Setters
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set
+            {
+                nombre = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Contra
+        {
+            get { return contra; }
+            set
+            {
+                contra = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion Getters & Setters
+
+
+    }
+}
