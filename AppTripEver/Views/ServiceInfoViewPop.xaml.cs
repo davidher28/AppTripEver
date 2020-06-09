145ff32a7a941e7c33f5591377656dcefab504a1
mclopez09 <mclopez09@javerianacali.edu.co>
2020-06-09 00:21:12 -0500

    ViewPop

---
+++ AppTripEver/Views/ServiceInfoViewPop.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using AppTripEver.ViewModels;
+using Xamarin.Forms.Xaml;
+
+namespace AppTripEver.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ServiceInfoViewPop
+    {
+        //MessageViewModel context = new MessageViewModel();
+        public ServiceInfoViewPop()
+        {
+            InitializeComponent();
+            //BindingContext = context;
+        }
+    }
+}
