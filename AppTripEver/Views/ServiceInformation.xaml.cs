84e43b14ae654123b45a4c96149cd849ee80cd8a
mclopez09 <mclopez09@javerianacali.edu.co>
2020-06-08 20:11:39 -0500

    Pagina Reservar

---
+++ AppTripEver/Views/ServiceInformation.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppTripEver.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppTripEver.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ServiceInformation : ContentPage
+    {
+        ServiceInformationViewModel context = new ServiceInformationViewModel();
+        public ServiceInformation()
+        {
+            InitializeComponent();
+            BindingContext = context;
+        }
+    }
+}
