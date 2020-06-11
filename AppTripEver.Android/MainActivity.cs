0394a62329427d2b6b6937a97360bb05ec2ed2f5
davidher28 <davidher28@javerianacali.edu.co>
2020-06-11 17:31:21 -0500

    Notificaciones Push

--- AppTripEver.Android/MainActivity.cs
+++ AppTripEver.Android/MainActivity.cs
@@ -6,10 +6,11 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Plugin.FirebasePushNotification;
 
 namespace AppTripEver.Droid
 {
-    [Activity(Label = "TripEver", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Portrait)]
+    [Activity(Label = "TripEver", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Portrait)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -22,6 +23,7 @@ namespace AppTripEver.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+            FirebasePushNotificationManager.ProcessIntent(this, Intent);
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
--- AppTripEver/App.xaml.cs
+++ AppTripEver/App.xaml.cs
@@ -2,6 +2,8 @@
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using AppTripEver.Views;
+using Plugin.FirebasePushNotification;
+
 namespace AppTripEver
 {
     public partial class App : Application
@@ -18,6 +20,12 @@ namespace AppTripEver
 
         protected override void OnStart()
         {
+            CrossFirebasePushNotification.Current.Subscribe("general");
+            CrossFirebasePushNotification.Current.OnTokenRefresh += (s, p) =>
+            {
+                System.Diagnostics.Debug.WriteLine($"TOKEN : {p.Token}");
+                Console.WriteLine("DAVID");
+            };
         }
 
         protected override void OnSleep()
